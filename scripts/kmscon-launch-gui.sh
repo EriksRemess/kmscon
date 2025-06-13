1846d5c5f149518d70d9da25c00200c9f6e30928
Jocelyn Falempe <jfalempe@redhat.com>
2025-06-13 14:49:14 +0200

    kmscon-launch-gui.sh: wait for tty to be active
    If you switch tty, and terminate the gui application, kmscon will
    steal the VT.
    So wait to switch back to the tty that it was launched on, before
    sending setForeground command to kmscon.
    
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

--- scripts/kmscon-launch-gui.sh
+++ scripts/kmscon-launch-gui.sh
@@ -5,6 +5,19 @@ if [ "$#" -eq 0 ]; then
     exit 1
 fi
 
+# Store current tty number
+if [ -d /sys/class/tty/tty0 ]; then
+    kms_tty=$(cat /sys/class/tty/tty0/active)
+fi
+
 printf "\x1B]setBackground\a"
 eval "${@:1}"
+
+# If the current tty has changed, wait until the user switches back.
+if [ -n "${kms_tty}" ]; then
+    while [ "$(cat /sys/class/tty/tty0/active)" != "${kms_tty}" ]; do
+        sleep 1
+    done
+fi
+
 printf "\x1B]setForeground\a"
